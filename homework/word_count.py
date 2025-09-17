58a49ec5586c516737efa26c7129c6dbafc8a352
jagiraldorestrepo <jugiraldore@unal.edu.co>
2025-09-17 10:04:06 -0500

    commit de prueba

--- homework/word_count.py
+++ homework/word_count.py
@@ -16,7 +16,6 @@ def copy_raw_files_to_input_folder(n):
     """Generate n copies of the raw files in the input folder"""
 
 
-
 def load_input(input_directory):
     """Funcion load_input"""
 
@@ -80,3 +79,6 @@ if __name__ == "__main__":
 
     end_time = time.time()
     print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
+
+
+print('prueba')
